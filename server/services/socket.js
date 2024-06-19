7bc2c9301dc3c997a1be214979576300f424f8df
Abhishek Shukla <111686252+Abhishek-2400@users.noreply.github.com>
2024-06-19 16:03:29 +0530

    Update socket.js

--- server/services/socket.js
+++ server/services/socket.js
@@ -1,15 +1,42 @@
+import Redis from "ioredis"
+import dotenv from "dotenv";
+dotenv.config();
+
+const pub = new Redis({
+    host: process.env.HOST,
+    port: parseInt(process.env.REDISPORT),
+    username: process.env.USER,
+    password: process.env.PASSWORD
+});
+
+const sub = new Redis({
+    host: process.env.HOST,
+    port: parseInt(process.env.REDISPORT),
+    username: process.env.USER,
+    password: process.env.PASSWORD
+});
+
+
 class SocketServices {
     constructor(io) {
         this.io = io
+        sub.subscribe("MESSAGES")
     }
     eventListeners() {
         this.io.on('connection', (socket) => {
             console.log(`User connected ${socket.id}`)
-            socket.on('message', (data) => {
+            socket.on('message-from-client', async (data) => {
                 console.log(`data received ${data}`)
+                await pub.publish('MESSAGES', JSON.stringify({ data }))
             })
             socket.on('disconnect', () => {
                 console.log(`User disconnected ${socket.id}`)
+            })
+
+            sub.on('message', (channel, message) => {
+                if (channel === "MESSAGES") {
+                    this.io.emit('message-from-redis', message)
+                }
             })
         })
     }
